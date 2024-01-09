b86705d1eebf8fc851c2eebddd63636c6175dc75
Osman Erdem <osmankilic10@gmail.com>
2024-01-09 16:59:54 +0300

    React Bootcamp - 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,60 @@
 import Text from "./components/Text"
 import Button from "./components/Button"
+import React, { useState } from "react"
 
 
 function App() {
 
- //props
+  const [name, setName] = useState(null)
+  //props
+  const [data, setData] = useState([])
+  const [count, setCount] = useState(0)
+
+  const clickFunc = () => {
+    alert("Click")
+    setName("react değişti")
+  }
+
+
+  const clickFunc2 = () => {
+    setData(prev => ([...prev, name]))
+  }
+
+  const decrement = () => {
+    if (count <= 0) return
+    setCount(prev => prev - 1);
+  }
+
+  console.log(data,"data");
+
+
   return (
-    <div>
-      react eğitimi 1
-      <Text number="2" name={"REACT"}></Text>
-      react eğitimi 3
-      <Text number="4" name={"REACTTT"}></Text>
-      <Text number="5" name={"REACTTTT"}></Text>
-      <Button name={"Sil"}/>
-      <Button name={"Gönder"}/>
-    </div>
+    <>
+      <div onClick={clickFunc}>
+        {name} <br></br>
+      </div>
+      <div>
+        react eğitimi 1
+        <Text number="2" name={"REACT"}></Text>
+        react eğitimi 3
+        <Text number="4" name={"REACTTT"}></Text>
+        <Text number="5" name={"REACTTTT"}></Text>
+        <Button name={"Azalt"} onClick={decrement}></Button>
+        {/* <button onClick={decrement} >Azalt</button> */}
+        <button onClick={() => setCount(count + 1)}>Arttır</button>
+        <div>{count}</div>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <div>{name}</div>
+        <button onClick={clickFunc2}>Tıkla</button>
+        <div>
+          {data.map((data, i) => {
+
+            <div key={i}>{data}</div>
+        })}
+        </div>
+        <div>{data}</div>
+      </div>
+    </>
   )
 }
 
